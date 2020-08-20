cdacdf2fb15f9340ffaff4ca2878d32a8621bde3
Marius Mitea <mmitea@trusimulation.com>
2020-08-20 13:21:50 -0400

    energy solver, awaiting energy db parsing

---
+++ GrimBuilding/Models/Solvers/TotalEnergySolver.cs
@@ -0,0 +1,24 @@
+﻿using GrimBuilding.Common.Support;
+using System;
+using System.Collections.Generic;
+
+namespace GrimBuilding.Solvers
+{
+    [SolverDependency(typeof(TotalSpiritSolver))]
+    public class TotalEnergySolver : SolverBase
+    {
+        const int EnergyPerSpiritPoint = 2;
+        const int BaseEnergyAt50Spirit = 250;
+
+        public override bool Solve(FullBuildModel fullBuild, BaseStats summedStats, Dictionary<Type, SolverResult> results, out SolverResult result)
+        {
+            var totalSpirit = results[typeof(TotalSpiritSolver)].Value;
+
+            var totalEnergy =
+                (summedStats.Energy + EnergyPerSpiritPoint * (totalSpirit - 50) + BaseEnergyAt50Spirit)
+                * (1 + summedStats.EnergyModifier / 100);
+            result = new SolverResult { Text = $"{totalEnergy:0} Total Energy", Value = totalEnergy };
+            return totalEnergy != 0;
+        }
+    }
+}
